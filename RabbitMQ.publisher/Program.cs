2c23838eea9daa9e60f7babbee65bd08305c22e1
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2022-09-21 07:39:16 +0300

    initial ,no exchange

---
+++ RabbitMQ.publisher/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMQ.publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            //channel.QueueDeclare("hello-queue", true, false, false);
+            channel.QueueDeclare(queue: "hello-queue",
+                    durable: true, //Data fiziksel olarak mı yoksa memoryde mi tutulsun
+                    exclusive: false, //Başka connectionlarda bu kuyruğa ulaşabilsin mi
+                    autoDelete: false, //Eğer kuyruktaki son mesaj ulaştığında kuyruğun silinmesini istiyorsak kullanılır.
+                    arguments: null//Exchangelere verilecek olan parametreler tanımlamak için kullanılır.
+                    );
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                string message = $"Message {x}";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                //channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+                //Queue ya atmak için kullanılır.
+                channel.BasicPublish(exchange: "",//mesajın alınıp bir veya birden fazla queue ya konmasını sağlıyor.
+                    routingKey: "hello-queue", //Hangi queue ya atanacak.
+                    body: messageBody//Mesajun içeriği
+                    );
+                Console.WriteLine($"Mesaj Gönderilmiştir : {message}");
+
+            });
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            //0=> herhangi bir boyuttaki değeri döndür
+            //1=> Mesajlar kaçar adet gelsin
+            //true => GelenMesaj/Subscriber ---- false => Gelen mesajları tek tek gönderir
+            channel.BasicQos(0, 1, false);
+
+            //channel.QueueDeclare("hello-queue", true, false, false);
+            channel.QueueDeclare(queue: "hello-queue",
+                    durable: true, //Data fiziksel olarak mı yoksa memoryde mi tutulsun
+                    exclusive: false, //Başka connectionlarda bu kuyruğa ulaşabilsin mi
+                    autoDelete: false, //Eğer kuyruktaki son mesaj ulaştığında kuyruğun silinmesini istiyorsak kullanılır.
+                    arguments: null//Exchangelere verilecek olan parametreler tanımlamak için kullanılır.
+                    );
+            var consumer = new EventingBasicConsumer(channel);
+
+            //channel.BasicConsume("hello-queue", true, consumer);
+            channel.BasicConsume(queue: "hello-queue", //TODO: Consume edilecek kuyruk ismi
+                    autoAck: false, //TODO: Gelen data doğru şekilde işlenirse kuyruktan silinir
+                    consumer: consumer//TODO: Hangi consumer kullanılacak.
+                    );
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine("Gelen Mesaj:" + message);
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+            Console.ReadLine();
+        }
+
+    }
+}
