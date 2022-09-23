623b11b5a2f178dccf0d710f04852594b3f60abc
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2022-09-23 16:15:11 +0300

    add controller(Account,Products)

---
+++ RabbitMQWeb.ExcelCreate/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitMQWeb.ExcelCreate.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
